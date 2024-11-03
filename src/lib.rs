c35088be7956b0387702ab18766b95292656217a
decaday <myDecaday@outlook.com>
2024-11-03 20:22:08 +0800

    feat: fmt mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,31 @@
 #![cfg_attr(not(test), no_std)]
 #![allow(async_fn_in_trait)]
 
+// This must go FIRST so that all the other modules see its macros.
+mod fmt;
+include!(concat!(env!("OUT_DIR"), "/_macros.rs"));
+
+mod macros;
+
+// This must go last, so that it sees all the impl_foo! macros defined earlier.
+pub(crate) mod _generated {
+    #![allow(dead_code)]
+    #![allow(unused_imports)]
+    #![allow(non_snake_case)]
+    #![allow(missing_docs)]
+
+    include!(concat!(env!("OUT_DIR"), "/_generated.rs"));
+}
+
+// pub use crate::_generated::interrupt;
+
+pub use _generated::{peripherals, Peripherals};
+pub use embassy_hal_internal::{into_ref, Peripheral, PeripheralRef};
 
 pub use py32_metapac as pac;
 
 pub mod time;
 
 pub mod rcc;
+
+pub mod gpio;
