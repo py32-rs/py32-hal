80833a65791ffd9f45d2ae553ceeb414229ecef6
decaday <myDecaday@outlook.com>
2024-11-04 12:13:26 +0800

    feat: GPIO🎉

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ include!(concat!(env!("OUT_DIR"), "/_macros.rs"));
 
 mod macros;
 
+pub use py32_metapac as pac;
+
 // This must go last, so that it sees all the impl_foo! macros defined earlier.
 pub(crate) mod _generated {
     #![allow(dead_code)]
@@ -22,10 +24,19 @@ pub(crate) mod _generated {
 pub use _generated::{peripherals, Peripherals};
 pub use embassy_hal_internal::{into_ref, Peripheral, PeripheralRef};
 
-pub use py32_metapac as pac;
-
 pub mod time;
-
 pub mod rcc;
-
 pub mod gpio;
+
+// pub fn init(config: Config) -> Peripherals {
+// }
+
+pub fn init() -> Peripherals {
+    critical_section::with(|cs| {
+        let p = Peripherals::take_with_cs(cs);
+        unsafe {
+            gpio::init(cs);
+        };
+        p
+    })
+}
