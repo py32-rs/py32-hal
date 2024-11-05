79905710624c4786255192a6355778c7f1c4e345
decaday <myDecaday@outlook.com>
2024-11-05 12:29:23 +0800

    example: add rcc example

--- examples/py32f030/src/bin/rcc_48mhz.rs
+++ examples/py32f030/src/bin/rcc_48mhz.rs
@@ -3,12 +3,19 @@
 
 use defmt::*;
 use py32_hal::gpio::{Level, Output, Speed};
+use py32_hal::rcc::{PllSource, Pll, Sysclk};
+use py32_hal::time::Hertz;
 use {defmt_rtt as _, panic_halt as _};
 use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    let p = py32_hal::init(Default::default());
+    let mut cfg: py32_hal::Config = Default::default();
+    cfg.rcc.hsi = Some(Hertz::mhz(24));
+    cfg.rcc.pll = Some(Pll { src: PllSource::HSI });
+    cfg.rcc.sys = Sysclk::PLL;
+    let p = py32_hal::init(cfg);
+
     info!("Hello World!");
 
     let mut led = Output::new(p.PB1, Level::High, Speed::Low);
