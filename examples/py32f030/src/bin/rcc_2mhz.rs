5ac263e9ac93bc7378dd1e3b1f285ac849ddad64
decaday <myDecaday@outlook.com>
2024-11-22 14:23:27 +0800

    example: add hsi div example

--- examples/py32f030/src/bin/rcc_2mhz.rs
+++ examples/py32f030/src/bin/rcc_2mhz.rs
@@ -4,8 +4,7 @@
 
 use defmt::*;
 use py32_hal::gpio::{Level, Output, Speed};
-use py32_hal::rcc::{Pll, PllSource, Sysclk};
-use py32_hal::time::Hertz;
+use py32_hal::rcc::Hsidiv;
 use embassy_executor::Spawner;
 use {defmt_rtt as _, panic_halt as _};
 
@@ -13,24 +12,22 @@ use {defmt_rtt as _, panic_halt as _};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let mut cfg: py32_hal::Config = Default::default();
-    cfg.rcc.hsi = Some(Hertz::mhz(24));
-    cfg.rcc.pll = Some(Pll {
-        src: PllSource::HSI,
-    });
-    cfg.rcc.sys = Sysclk::PLL;
+    // cfg.rcc.hsi = Some(Hertz::mhz(8)); // default
+    cfg.rcc.hsidiv = Hsidiv::DIV4;
+    // cfg.rcc.sys = Sysclk::HSI; // default
     let p = py32_hal::init(cfg);
 
     info!("Hello World!");
 
-    let mut led = Output::new(p.PB1, Level::High, Speed::Low);
+    let mut led = Output::new(p.PA6, Level::High, Speed::Low);
 
     loop {
         info!("high");
         led.set_high();
-        cortex_m::asm::delay(8_000_000);
+        cortex_m::asm::delay(2_000_000);
 
         info!("low");
         led.set_low();
-        cortex_m::asm::delay(8_000_000);
+        cortex_m::asm::delay(2_000_000);
     }
 }
