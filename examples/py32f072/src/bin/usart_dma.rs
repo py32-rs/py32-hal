2eccbd4cb4af6ce3e5d7463e393e2e5e6712cc6b
decaday <myDecaday@outlook.com>
2025-01-11 17:18:05 +0800

    examples: add f030 f072 usart dma & usart ringbuffered examples

---
+++ examples/py32f072/src/bin/usart_dma.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+#![feature(impl_trait_in_assoc_type)]
+
+use core::fmt::Write;
+
+use defmt::*;
+use embassy_executor::Spawner;
+use py32_hal::usart::{Config, Uart};
+use py32_hal::{bind_interrupts, peripherals, usart};
+use heapless::String;
+use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    USART1 => usart::InterruptHandler<peripherals::USART1>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = py32_hal::init(Default::default());
+    info!("Hello World!");
+
+    let config = Config::default();
+    let mut usart = Uart::new(p.USART1, p.PA10, p.PA9, Irqs, p.DMA1_CH3, p.DMA1_CH1, config).unwrap();
+
+    for n in 0u32.. {
+        let mut s: String<128> = String::new();
+        core::write!(&mut s, "Hello DMA World {}!\r\n", n).unwrap();
+
+        unwrap!(usart.write(s.as_bytes()).await);
+        info!("wrote DMA");
+    }
+}
