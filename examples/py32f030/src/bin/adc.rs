17eca05097a06976d20b6999faa622a18a1daae3
decaday <myDecaday@outlook.com>
2025-08-15 17:39:34 +0800

    Fix ADC v1 driver code, add v1b support

--- examples/py32f030/src/bin/adc.rs
+++ examples/py32f030/src/bin/adc.rs
@@ -24,17 +24,18 @@ async fn main(_spawner: Spawner) {
     let mut pin = p.PA1;
 
     let mut vrefint = adc.enable_vref();
-    let vrefint_sample = adc.read(&mut vrefint).await;
-    let convert_to_millivolts = |sample| {
-        const VREFINT_MV: u32 = 1200; // mV
-
-        (u32::from(sample) * VREFINT_MV / u32::from(vrefint_sample)) as u16
-    };
 
     loop {
+        let vrefint_sample = adc.read(&mut vrefint).await;
+        let convert_to_millivolts = |sample| {
+            const VREFINT_MV: u32 = 1200; // mV
+
+            (u32::from(sample) * VREFINT_MV / u32::from(vrefint_sample)) as u16
+        };
+
         let v = adc.read(&mut pin).await;
         info!("vrefint_sample: {}", vrefint_sample);
         info!("--> {} - {} mV", v, convert_to_millivolts(v));
-        Timer::after_millis(100).await;
+        Timer::after_millis(500).await;
     }
 }
--- src/adc/mod.rs
+++ src/adc/mod.rs
@@ -8,6 +8,7 @@
 // Special thanks to the Embassy Project and its contributors for their work!
 
 #[cfg_attr(adc_v1, path = "v1.rs")]
+#[cfg_attr(adc_v1b, path = "v1.rs")]
 #[cfg_attr(adc_v2, path = "v2.rs")]
 mod _version;
 
