3424241b96ae1a30bd80967e4c57d8539c105537
Jie Feng <hjf3108@gmail.com>
2026-07-24 11:03:47 +0800

    remove separate f071

--- examples/py32f071/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    // `--nmagic` is required because the memory sections are not aligned to
-    // 0x10000 boundaries.
-    println!("cargo:rustc-link-arg=--nmagic");
-    println!("cargo:rustc-link-arg=-Tlink.x");
-    println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
-}
